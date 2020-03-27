2bb68358ee4dc1366fe0627ae7b78fb332274007
Andrew Sargeant <agdsargeant@gmail.com>
2020-03-27 16:00:06 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -3,7 +3,7 @@ require 'directors_database'
 # Call the method directors_database to retrieve the NDS
 
 require 'pp'
-directors_database
+ddb = directors_database
 
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
@@ -11,6 +11,15 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
+  first_director_films = ddb[0][:movies] #array of hashes
+  array_index = 0
+  output_array = []
+  while array_index < first_director_films.length do
+    output_array.push(first_director_films[array_index][:title])
+    array_index += 1
+  end
+  return output_array
+  
 end
 
 pretty_print_nds(directors_database)
